f845cf8753712c548381127445c379a6a7596405
Andry Polo <adpolog@unimagdalena.edu.co>
2026-08-23 22:45:44 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,13 @@ public class Main {
     public static void main(String[] args) {
         
         System.out.printf("MIS CAMBIOS EN GIT!");
+         String nombre = "Andry";
+        int edad = 19;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println();
+
 
         for (int i = 1; i <= 5; i++) {
            
